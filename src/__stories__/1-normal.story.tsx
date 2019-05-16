77f4d129fa0ba59c167968c7d596b37e2c51983f
areschen <ariesjia00@hotmail.com>
2019-05-16 23:57:45 +0800

    test: 💍 add thunk middleware test

--- src/__stories__/1-normal.story.tsx
+++ src/__stories__/1-normal.story.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import {storiesOf} from '@storybook/react'
-import {action} from '@storybook/addon-actions'
 
 import useMiddleWares from '../index'
 
---
+++ src/__tests__/thunk-middleware.test.tsx
@@ -0,0 +1,88 @@
+import * as React from "react";
+import render, { act } from 'hooks-test-util'
+import thunkMiddleware from 'redux-thunk'
+import useMiddleWares from '../index'
+
+
+function flushPromises() {
+  return new Promise(function(resolve) {
+    setTimeout(resolve);
+  });
+}
+
+describe("middleware test",() => {
+  it('should excute middleware when dispatch', async () => {
+    const mockFunc = jest.fn()
+
+    const initialState = {count: 0};
+
+    function reducer(state, action) {
+      switch (action.type) {
+        case 'increment':
+          return {count: state.count + 1};
+        case 'decrement':
+          return {count: state.count - 1};
+        case 'reset':
+          return {count: 0};
+        default:
+          return state
+      }
+    }
+
+    const logMiddleware = ({ getState }) => next => action => {
+      mockFunc(getState())
+      next(action)
+      mockFunc(getState())
+    }
+
+    const { container } = render(() => useMiddleWares(reducer, initialState)([
+      thunkMiddleware, logMiddleware
+    ]))
+
+    act(() => {
+      container.hook[1]((dispatch) => {
+        dispatch({type: 'increment'})
+        Promise.resolve().then(() => {
+          dispatch({type: 'reset'})
+        })
+      })
+    })
+
+    act(() => {
+      container.hook[1]((dispatch) => {
+        dispatch({type: 'increment'})
+        Promise.resolve().then(() => {
+          dispatch({type: 'reset'})
+        })
+      })
+    })
+
+    await flushPromises()
+
+    expect(mockFunc).toBeCalledTimes(8)
+    expect(mockFunc).toHaveBeenNthCalledWith(1, {
+      count: 0
+    })
+    expect(mockFunc).toHaveBeenNthCalledWith(2, {
+      count: 1
+    })
+    expect(mockFunc).toHaveBeenNthCalledWith(3, {
+      count: 1
+    })
+    expect(mockFunc).toHaveBeenNthCalledWith(4, {
+      count: 2
+    })
+    expect(mockFunc).toHaveBeenNthCalledWith(5, {
+      count: 2
+    })
+    expect(mockFunc).toHaveBeenNthCalledWith(6, {
+      count: 0
+    })
+    expect(mockFunc).toHaveBeenNthCalledWith(7, {
+      count: 0
+    })
+    expect(mockFunc).toHaveBeenNthCalledWith(8, {
+      count: 0
+    })
+  })
+})
